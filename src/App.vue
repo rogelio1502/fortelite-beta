482ae5ecc35bca54a4b55d414813658c8aefef53
rogelio1502 <rogelio15021515@gmail.com>
2024-07-21 16:52:02 -0600

    added wha icon

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="wha-icon">
+    <a
+      href="https://api.whatsapp.com/send?phone=528135433600"
+      target="_blank"
+      title="Enviar Mensaje"
+      ><i class="fab fa-whatsapp"></i
+    ></a>
+  </div>
   <DefaultLayout>
     <RouterView />
   </DefaultLayout>
@@ -7,5 +15,24 @@
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import "@/assets/main.css";
 </script>
-<style lang="scss">
+
+<style >
+
+.wha-icon {
+  position: fixed;
+  z-index: 100;
+  bottom: 0px;
+  right: 10px;
+}
+.wha-icon a {
+  font-size: 75px;
+}
+.wha-icon a :hover {
+  font-size: 90px;
+  transition: 1s;
+}
+.wha-icon a i {
+  color: #25D366;
+}
+
 </style>
