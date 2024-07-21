b2da54c1ec59bb2a5f7c10a227bd3674c2698592
rogelio1502 <rogelio15021515@gmail.com>
2024-07-21 16:28:22 -0600

    contact us

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,4 +1,5 @@
 export const CONTACT = {
     title: "Contacto",
     buttom_image: "https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/Captura-de-pantalla-2024-07-21-a-las-12.46.58%E2%80%AFp.m-e1721587699795.png",
+    top_banner: "https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/Captura-de-pantalla-2024-07-21-a-las-3.01.31%E2%80%AFp.m.png",
 }
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,99 +1,147 @@
-<script setup>
-import { computed } from 'vue';
-import { CONTACT} from "@/constants/contact";
-
-const buttomImage = computed(() => CONTACT.buttom_image);
-const title = computed(() => CONTACT.title);
-</script>
 <template>
     <section class="bg-primary w-full">
-        Foto de los 3 men
+        <img :src="topBannerImage" alt="">
     </section>
-    <section class="bg-primary w-full">
-        Slider
-    </section>
+
     <section class="bg-white w-full">
-       <section class="py-10">
+        <section class="py-10">
             <h1 class="text-center text-5xl font-bold text-sky-200">{{ title }}</h1>
-       </section>
-       <section class="w-full bg-sky-100 flex justify-center ">
-            <form class="p-20 rounded-lg w-4/5">
+        </section>
+        <section class="w-full bg-sky-100 flex justify-center ">
+            <form @submit.prevent="handleSubmit" class="p-20 rounded-lg w-4/5">
                 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 mb-4">
                     <div>
-                        <label for="nombre-completo" class="block text-gray-700 mb-2">Nombre completo</label>
-                        <input type="text" id="nombre-completo" class="border border-gray-300 p-2 w-full rounded">
+                        <label for="fullName" class="block text-gray-700 mb-2">Nombre completo</label>
+                        <input type="text" id="fullName" v-model="formData.fullName"
+                            class="border border-gray-300 p-2 w-full rounded" required minlength="3" />
                     </div>
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                     <div>
-                        <label for="correo" class="block text-gray-700 mb-2">Correo</label>
-                        <input type="email" id="correo" class="border border-gray-300 p-2 w-full rounded">
+                        <label for="email" class="block text-gray-700 mb-2">Correo</label>
+                        <input type="email" id="email" v-model="formData.email"
+                            class="border border-gray-300 p-2 w-full rounded" required />
                     </div>
                     <div>
-                        <label for="telefono" class="block text-gray-700  mb-2">Teléfono</label>
-                        <input type="tel" id="telefono" class="border border-gray-300 p-2 w-full rounded">
+                        <label for="phone" class="block text-gray-700 mb-2">Teléfono</label>
+                        <input type="tel" id="phone" v-model="formData.phone"
+                            class="border border-gray-300 p-2 w-full rounded" pattern="[0-9]{10}" required />
                     </div>
                 </div>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                     <div>
                         <div class="mb-4">
-                            <label for="estado" class="block text-gray-700 mb-2">Estado</label>
-                            <input type="text" id="estado" class="border border-gray-300 p-2 w-full rounded">
+                            <label for="state" class="block text-gray-700 mb-2">Estado</label>
+                            <input type="text" id="state" v-model="formData.state"
+                                class="border border-gray-300 p-2 w-full rounded" required />
                         </div>
                         <div class="mb-4">
-                            <label for="sector-mercado" class="block text-gray-700  mb-2">Sector de mercado</label>
-                            <input type="text" id="sector-mercado" class="border border-gray-300 p-2 w-full rounded" >
+                            <label for="marketSector" class="block text-gray-700 mb-2">Sector de mercado</label>
+                            <input type="text" id="marketSector" v-model="formData.marketSector"
+                                class="border border-gray-300 p-2 w-full rounded" required />
                         </div>
                         <div class="mb-4">
-                            <label for="cantidad-piezas" class="block text-gray-700  mb-2">Cantidad de piezas</label>
-                            <input type="number" id="cantidad-piezas" class="border border-gray-300 p-2 w-full rounded" >
+                            <label for="piecesQuantity" class="block text-gray-700 mb-2">Cantidad de piezas</label>
+                            <input type="number" id="piecesQuantity" v-model="formData.piecesQuantity"
+                                class="border border-gray-300 p-2 w-full rounded" min="1" required />
                         </div>
                     </div>
                     <div>
-                        <label class="block text-gray-700  mb-2 mt-8">¿Cómo fue que nos conociste?</label>
+                        <label class="block text-gray-700 mb-2 mt-8">¿Cómo fue que nos conociste?</label>
                         <div class="flex flex-col">
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
                                 <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Google" class="mr-2"> Google
+                                    <input type="radio" name="origin" value="Google" v-model="formData.origin"
+                                        class="mr-2" required /> Google
                                 </label>
                                 <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Linkedin" class="mr-2"> Linkedin
-                                </label>
-                            </div>
-                            <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
-                                <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Facebook" class="mr-2"> Facebook
-                                </label>
-                                <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Instagram" class="mr-2"> Instagram
+                                    <input type="radio" name="origin" value="Linkedin" v-model="formData.origin"
+                                        class="mr-2" /> Linkedin
                                 </label>
                             </div>
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
                                 <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Recomendacion" class="mr-2"> Recomendación
+                                    <input type="radio" name="origin" value="Facebook" v-model="formData.origin"
+                                        class="mr-2" /> Facebook
                                 </label>
                                 <label class="mb-2">
-                                    <input type="radio" name="conociste" value="Otro" class="mr-2"> Otro
+                                    <input type="radio" name="origin" value="Instagram" v-model="formData.origin"
+                                        class="mr-2" /> Instagram
+                                </label>
+                            </div>
+                            <div class="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
+                                <label class="mb-2">
+                                    <input type="radio" name="origin" value="Recomendacion"
+                                        v-model="formData.origin" class="mr-2" /> Recomendación
+                                </label>
+                                <label class="mb-2">
+                                    <input type="radio" name="origin" value="Otro" v-model="formData.origin"
+                                        class="mr-2" /> Otro
                                 </label>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="mb-4">
-                    <label for="mensaje" class="block text-gray-700  mb-2">Mensaje</label>
-                    <textarea id="mensaje" class="border border-gray-300 p-2 w-full rounded" rows="4" ></textarea>
+                    <label for="message" class="block text-gray-700 mb-2">Mensaje</label>
+                    <textarea id="message" v-model="formData.message" class="border border-gray-300 p-2 w-full rounded"
+                        rows="4" required></textarea>
                 </div>
                 <div class="flex justify-end">
-                    <button type="submit" class="bg-primary text-white  py-1 px-8 hover:bg-blue-700">Enviar</button>
+                    <button type="submit" class="bg-primary text-white py-1 px-8 hover:bg-blue-700">Enviar</button>
                 </div>
             </form>
-       </section>
-       <section class="w-full">
+        </section>
+        <section class="w-full">
             <img :src="buttomImage" width="300">
-       </section>
+        </section>
     </section>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { ref } from 'vue';
+import { CONTACT } from '@/constants/contact';
+
+const buttomImage = computed(() => CONTACT.buttom_image);
+const title = computed(() => CONTACT.title);
+const topBannerImage = computed(() => CONTACT.top_banner);
+
+const formData = ref({
+    fullName: '',
+    email: '',
+    phone: '',
+    state: '',
+    marketSector: '',
+    piecesQuantity: '',
+    origin: '',
+    message: ''
+});
+
+const handleSubmit = () => {
+    // Validate and handle form submission
+    console.log('Form data:', formData.value);
+    // Example validation
+    for (const key in formData.value) {
+        if (formData.value[key] === '') {
+            alert(`El campo ${key} es requerido.`);
+            return;
+        }
+    }
+    formData.value = {
+        fullName: '',
+        email: '',
+        phone: '',
+        state: '',
+        marketSector: '',
+        piecesQuantity: '',
+        origin: '',
+        message: ''
+    };
+    alert('¡Muchas gracias por contactarnos, estaremos en contacto contigo lo más pronto posible!');
+};
+</script>
+
 <style scoped>
-
+/* Optional: additional styles */
 </style>
-
