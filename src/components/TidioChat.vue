2ad555b923adcd8caf257d0310766a1d61428986
rogelio1502 <rogelio15021515@gmail.com>
2024-07-25 19:55:17 -0600

    tidio chat integration

---
+++ src/components/TidioChat.vue
@@ -0,0 +1,22 @@
+<!-- src/components/TidioChat.vue -->
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  import { MAIN } from '@/constants/main';
+  export default {
+    mounted() {
+      this.loadTidioScript();
+    },
+    methods: {
+      loadTidioScript() {
+        const tidioScript = document.createElement('script');
+        tidioScript.src = 'https://code.tidio.co/' + MAIN.tidio_code + '.js';
+        tidioScript.async = true;
+        document.body.appendChild(tidioScript);
+      }
+    }
+  }
+  </script>
+  
