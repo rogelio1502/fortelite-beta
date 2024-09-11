deab876964a7c81504d443f34ff8f25b6abf2b31
MrD1scr3t <rodrigogaelsal@gmail.com>
2024-09-11 11:32:42 -0600

    Se arregla header mobile

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -44,7 +44,7 @@
         v-for="(navItem, navItemIndex) in navigationList"
         :key="`${navItem.text}_${navItemIndex}`"
         :to="navItem.to"
-        class="text-black bg-blue-700 p-2 rounded"
+        class="text-black  p-2 rounded"
       >
         {{ navItem.text }}
       </RouterLink>
